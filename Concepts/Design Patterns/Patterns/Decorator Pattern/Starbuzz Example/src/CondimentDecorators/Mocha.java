8975dfcdff9a2df447e6be99194798d160c1a536
Mike Coopman <macoopman@gmail.com>
2020-02-15 12:38:16 -0600

    added decorator example code

---
+++ Concepts/Design Patterns/Patterns/Decorator Pattern/Starbuzz Example/src/CondimentDecorators/Mocha.java
@@ -0,0 +1,4 @@
+package CondimentDecorators;
+
+public class Mocha {
+}
--- Concepts/Design Patterns/Patterns/Decorator Pattern/src/CondimentDecorators/CondimentDecorator.java
+++
@@ -1,7 +0,0 @@
-package CondimentDecorators;
-
-import BaseClasses.Beverage;
-
-public abstract class CondimentDecorator extends Beverage {
-   public abstract String getDescription();
-}
